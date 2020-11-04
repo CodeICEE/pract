ca7e21bdded8544ae1593831aa3ca820ce6a80c3
lopesmp <lopesmp@mail.broward.edu>
2020-11-03 19:59:55 -0500

    added Michele

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Vlad from'./components/Vlad';
+import Michele from './components/Michele';
 
 function App() {
   return (
+    <>
    <Vlad />
+   <Michele />
+   </>
   );
 }
 
